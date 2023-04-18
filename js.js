7b9b74bc810129453369c44b145b1ac2c5c6b45d
Thanh27 <95127658+SyThanhh@users.noreply.github.com>
2023-04-18 09:12:23 +0700

    upload code

---
+++ js.js
@@ -0,0 +1,66 @@
+const input = document.querySelector('input')
+const btnSha = document.querySelector('.btn-SHA-1')
+const btnMd5 = document.querySelector('.btn-MD5')
+const textarea  = document.querySelector('textarea')
+const icon = document.querySelector('.icon')
+
+function handleSHA1 () {
+    let value = input.value.trim()
+    if(!value) {
+        alert("Hãy nhập nội dung vào !")
+        return false;
+    }
+    let type = btnSha.textContent
+    let hash = CryptoJS.SHA1(value).toString()
+    let bit = hash.length * 4;
+    let message = input.value
+    // let byte = hash.length * 0.5;
+    // lưu dữ liệu
+    const text = {
+        type : type,
+        hash : hash,
+        bit : bit,
+        message : message
+    }
+
+    let content = `Type : ${text.type}
+    Hex : ${text.hash}
+    Bit : ${text.bit}
+    Message: ${text.message}`
+   
+    textarea.innerText = content
+}   
+function handleMD5 () {
+    let value = input.value.trim()
+    if(!value) {
+        alert("Hãy nhập nội dung vào !")
+        return false;
+    }
+    let type = btnMd5.textContent
+    let hash = CryptoJS.MD5(value).toString()
+    let bit = hash.length * 4;
+    let message = input.value
+    // let byte = hash.length * 0.5;
+
+    // lưu dữ liệu
+    const text = {
+        type : type,
+        hash : hash,
+        bit : bit,
+        message : message
+    }
+    let p = document.createElement("p");
+    p = `Type : ${text.type}
+            Hex : ${text.hash}
+            Bit : ${text.bit}
+            Message: ${text.message}
+    `
+    
+    textarea.innerText = p
+  
+}
+btnSha.addEventListener('click', () => handleSHA1())
+btnMd5.addEventListener('click', () => handleMD5())
+icon.onclick = function () {
+    input.value = ''
+}
